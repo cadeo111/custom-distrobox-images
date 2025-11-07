4506cc3f21298eee2cedc0eb54663ec6e7e85dc3
Caden Keese <github@keese.dev>
2025-11-07 20:35:15 +0100

    Update fedora-cosmic.sh

--- scripts/fedora-cosmic.sh
+++ scripts/fedora-cosmic.sh
@@ -13,6 +13,16 @@ sudo dnf install dnf5-plugins
 sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf install gh --repo gh-cli
 
+
+
+# install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"
+
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+cargo binstall --no-confirm eza du-dust 
+
 # Update the container and install packages again
 dnf update -y
 grep -v '^#' ./fedora-cosmic.packages | xargs dnf install -y
